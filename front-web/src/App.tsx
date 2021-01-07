47e98c74ae863ee99779a5f300175c4b036e894f
fagnercsouza <fagner.cicero@gmail.com>
2021-01-07 14:01:57 -0300

    Add HomePage

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Home';
 import NavBar from './NavBar'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <div>
       <NavBar/>
-      
+      <Home/>
     </div>
   );
 }
